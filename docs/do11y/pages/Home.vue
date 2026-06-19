cb6f109afcfc273f4e3400071710d1b05e4cc317
Norah <sq11y@proton.me>
2026-06-19 16:00:28 +0200

    Update quote on home page

--- docs/do11y/pages/Home.vue
+++ docs/do11y/pages/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <h1 class="tagline">
-    A component library for those who believe <em>everything</em> is political.
-  </h1>
+  <blockquote>
+    <h1 class="tagline">
+      "The power of the Web is in its universality. Access by everyone regardless of disability is
+      an essential aspect"
+    </h1>
+    <footer>
+      -
+      <!-- prettier-ignore -->
+      <cite>
+        Tim Berners-Lee,
+        <a href="https://www.w3.org/WAI/fundamentals/accessibility-intro/">W3C</a> Director and inventor of the World Wide Web
+      </cite>
+    </footer>
+  </blockquote>
 </template>
 
 <style lang="scss">
@@ -10,7 +21,19 @@
 .tagline {
   font-family: var(--font-family-heading);
   font-weight: var(--font-weight-bold);
-  font-size: var(--font-size-xxxxxl);
-  line-height: var(--line-height-xxxxxl);
+  font-size: var(--font-size-xxxxl);
+  line-height: var(--line-height-xxxxl);
+
+  @media (width < 32rem) {
+    text-align: center;
+    font-size: var(--font-size-xxxl);
+    line-height: var(--line-height-xxxl);
+  }
+}
+
+blockquote footer {
+  text-align: end;
+  font-size: var(--font-size-l);
+  line-height: var(--line-height-l);
 }
 </style>
